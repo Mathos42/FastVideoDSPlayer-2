33d0170a80fc687e71d1c8de0aa8d6658b318693
Mathos <rvlogiciels@gmail.com>
2026-09-14 16:15:16 +0200

    Update BrowserView.cpp

--- arm9/source/gui/BrowserView.cpp
+++ arm9/source/gui/BrowserView.cpp
@@ -107,6 +107,5 @@ void BrowserView::Render(const char* dirPath, const fv_dir_entry_t* entries, u32
     else
         iprintf("%lu entree(s)", (unsigned long)total);
 
-    iprintf("\x1b[13;0HBOUCLE:%s ALEA:%s START/SELECT", loopEnabled ? "ON " : "OFF", randomEnabled ? "ON " : "OFF");
     iprintf("\x1b[23;0H\x1b[36mA:OUVRIR B:RETOUR ST/SE:MODES\x1b[0m");
 }
